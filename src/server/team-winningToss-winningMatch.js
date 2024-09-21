afa2e8abe91b28639c5537298ede7c27d2ed2729
Shounak Mandal <shounak.mandal16@gmail.com>
2024-09-21 11:41:40 +0530

    updated a file

--- src/server/team-winningToss-winningMatch.js
+++ src/server/team-winningToss-winningMatch.js
@@ -2,20 +2,12 @@
 function tossMatch(arr){
 let win = {}; let tossWin = {}; let res = {}
   for(let ele of arr){
-   if(!(ele.toss_winner in tossWin))
-    tossWin[ele.toss_winner] = 1
-   else
-    tossWin[ele.toss_winner]++   
-   if(!(ele.winner in win))
-    win[ele.winner] = 1
-   else
-    win[ele.winner]++
+    if(!(ele.toss_winner in res))
+      res[ele.toss_winner] = 0
+    if((ele.toss_winner==ele.winner))
+      res[ele.toss_winner]++
   }
-  for(let ele in win){
-   if(ele!=="")
-   res[ele] = {wins: win[ele], tossWins: tossWin[ele]}
-  }
-  return res
+   return res
 }
 
 export {tossMatch}
