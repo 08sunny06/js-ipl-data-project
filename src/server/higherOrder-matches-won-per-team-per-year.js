43d3f4318af9b04eb7416bc239a7a8059a76d9c3
Shounak Mandal <shounak.mandal16@gmail.com>
2024-09-24 16:56:12 +0530

    updated and corrected my naming of higherOrder-matches-won-per-team-per-year.js

--- src/server/higherOrder-matches-won-per-team-per-year.js
+++ src/server/higherOrder-matches-won-per-team-per-year.js
@@ -3,8 +3,8 @@ import fsc from "fs"
 let data = fsc.readFileSync("/home/shounak/js-ipl-data-project/src/data/matches.json")
 let match = JSON.parse(data)
 
-function winningteam(arr){
-    let ar = arr.reduce((tot,cur) => {
+function winningteam(matches_ar){
+    let matches_won_per_team_per_year = matches_ar.reduce((tot,cur) => {
         let {season,winner} = cur
         if(!(season in tot))
             tot[season] = {}
@@ -15,7 +15,7 @@ function winningteam(arr){
         
         return tot
     },{})
-    return ar
+    return matches_won_per_team_per_year
 }
 
 console.log(winningteam(match))
