b779683bf5dfa9652ce8db9943c7ba7ace14d045
Shounak Mandal <shounak.mandal16@gmail.com>
2024-09-24 16:51:58 +0530

    updated and corrected my naming of team-winningToss-winningMatch.js

--- src/server/team-winningToss-winningMatch.js
+++ src/server/team-winningToss-winningMatch.js
@@ -1,13 +1,13 @@
 
-function tossMatch(arr){
-let win = {}; let tossWin = {}; let res = {}
-  for(let ele of arr){
-    if(!(ele.toss_winner in res))
-      res[ele.toss_winner] = 0
-    if((ele.toss_winner==ele.winner))
-      res[ele.toss_winner]++
+function tossMatch(matches_ar){
+let answer = {}
+  for(let matches_element of matches_ar){
+    if(!(matches_element.toss_winner in answer))
+      answer[matches_element.toss_winner] = 0
+    if((matches_element.toss_winner==matches_element.winner))
+      answer[matches_element.toss_winner]++
   }
-   return res
+   return answer
 }
 
 export {tossMatch}
