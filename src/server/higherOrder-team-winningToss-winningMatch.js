b7cea39717fec768971bf779b652dbb0bc018721
Shounak Mandal <shounak.mandal16@gmail.com>
2024-09-24 16:53:44 +0530

    Created and used correct naming variaqbles of higherOrder-team-winningToss-winningMatch.js

--- src/server/higherOrder-team-winningToss-winningMatch.js
+++ src/server/higherOrder-team-winningToss-winningMatch.js
@@ -2,13 +2,13 @@ import fil from "fs"
 let data = fil.readFileSync("/home/shounak/js-ipl-data-project/src/data/matches.json")
 let match = JSON.parse(data)
 
-function tossMatchWin(arr){
-    let res = arr.reduce((tot,cur) => {
-        let {toss_winner :tossWin, winner: win} = cur
-        if(!(tossWin in tot))
-            tot[tossWin] = 0
-        if(tossWin==win){
-            tot[tossWin]++
+function tossMatchWin(matches_ar){
+    let res = matches_ar.reduce((tot,matches_ar_data) => {
+        let {toss_winner , winner} = matches_ar_data
+        if(!(toss_winner in tot))
+            tot[toss_winner] = 0
+        if(toss_winner==winner){
+            tot[toss_winner]++
         }return tot
     },{})
     return res
