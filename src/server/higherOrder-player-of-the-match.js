48bb6a86f6113a4aa07674b708c37228fefcc8b8
Shounak Mandal <shounak.mandal16@gmail.com>
2024-09-24 16:54:44 +0530

    updated and corrected my naming of higherOrder-player-of-the-match.js

--- src/server/higherOrder-player-of-the-match.js
+++ src/server/higherOrder-player-of-the-match.js
@@ -2,26 +2,27 @@ import fil from "fs"
 let data = fil.readFileSync("/home/shounak/js-ipl-data-project/src/data/matches.json")
 let match = JSON.parse(data)
 
-function playerMatch(arr){
-    let res = arr.reduce((tot,cur) => {
-        let {season, player_of_match: player} = cur;
+function playerMatch(matches_ar){
+    let no_of_times_man_of_the_match = matches_ar.reduce((tot,matches_element) => {
+        let {season, player_of_match} = matches_element;
         if(!(season in tot))
             tot[season] = {}
-        if(!(player in tot[season]))
-            tot[season][player] = 1
+        if(!(player_of_match in tot[season]))
+            tot[season][player_of_match] = 1
         else
-            tot[season][player]++
+            tot[season][player_of_match]++
         return tot
     }, {})
-    let max = 0; let keys = Object.keys(res); 
-    let values = Object.values(res).reduce((tot,cur,ind)=>{
-        let key = Object.keys(cur);
-        let value = Object.values(cur)
-        let max = Math.max(...value)
-        tot[keys[ind]] = (key[value.indexOf(max)])
+    
+    let highest_player_of_match_count = 0; let keys = Object.keys(no_of_times_man_of_the_match); 
+    let values = Object.values(no_of_times_man_of_the_match).reduce((tot,man_of_match_data,ind)=>{
+        let key = Object.keys(man_of_match_data);
+        let value = Object.values(man_of_match_data)
+        let highest_player_of_match_count = Math.max(...value)
+        tot[keys[ind]] = (key[value.indexOf(highest_player_of_match_count)])
         return tot
     },{})
-    return values  
+     return values  
 }
 
 console.log(playerMatch(match))
