a182ccdc58b0c1eb17e338b12bc066581aa0465b
Shounak Mandal <shounak.mandal16@gmail.com>
2024-09-24 16:55:42 +0530

    updated and corrected my naming of higherOrder-matches-per-year.js

--- src/server/higherOrder-matches-per-year.js
+++ src/server/higherOrder-matches-per-year.js
@@ -3,15 +3,15 @@ import file from "fs"
 let data = file.readFileSync("/home/shounak/js-ipl-data-project/src/data/matches.json")
 let match = JSON.parse(data)
 
-function segrMatch(arr){
-    let ar = arr.reduce((tot,cur) => {
-        let {season} = cur
+function segrMatch(matches_ar){
+    let matches_per_yer_ar = matches_ar.reduce((tot,matches_element) => {
+        let {season} = matches_element
         if(!(season in tot))
             tot[season] = 0
         tot[season]++
         return tot
     },{})
-    return ar
+    return matches_per_yer_ar
 }
 
  console.log(segrMatch(match))
