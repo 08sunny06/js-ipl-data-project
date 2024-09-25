d42a50a8c3d2fbdfbb14281103dbe9d505c2a03e
Shounak Mandal <shounak.mandal16@gmail.com>
2024-09-25 16:37:45 +0530

    updated and refactored my extra-runs-conceded-per-team.js file

--- src/server/extra-runs-conceded-per-team.js
+++ src/server/extra-runs-conceded-per-team.js
@@ -1,26 +1,25 @@
 
 
-function extrRun(arr1, arr2){
-   let idMatch = []; let reqMatch = []; let extra = {}
-   for(let ele of arr1){
-    if(ele.season == 2016){
-        idMatch.push(ele)
+function extrRun(matches_ar, deliveries_ar){
+   let id_matches_ar = []; let req_data_of_deliveries_ar = []; let extra_runs = {}
+   for(let data_of_matches_ar of matches_ar){
+    if(data_of_matches_ar.season == 2016){
+        id_matches_ar.push(data_of_matches_ar)
     }
-   }
-   for(let play of idMatch){
-    for(let play1 of arr2){
-     if(play.id === play1.match_id)
-      reqMatch.push(play1)
+   }   
+   for(let ids_of_matches_ar of id_matches_ar){
+    for(let data_deliveries_ar of deliveries_ar){
+     if(ids_of_matches_ar.id === data_deliveries_ar.match_id)
+      req_data_of_deliveries_ar.push(data_deliveries_ar)
     }
-   }
-   for(let ele of reqMatch){
-    let exrun = 0 
-    if(!(ele.bowling_team in extra))
-       extra[ele.bowling_team] = 0
+   }   
+   for(let data_of_deliveries_ar of req_data_of_deliveries_ar){
+    if(!(data_of_deliveries_ar.bowling_team in extra_runs))
+       extra_runs[data_of_deliveries_ar.bowling_team] = 0
     else
-       extra[ele.bowling_team] += parseInt(ele.extra_runs)
+       extra_runs[data_of_deliveries_ar.bowling_team] += parseInt(data_of_deliveries_ar.extra_runs)
    }
-  return extra
+   return extra_runs
 }
 
 export {extrRun}
